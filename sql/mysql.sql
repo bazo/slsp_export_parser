e9a4c8dc1718f90971e4e5c140b2c3869d66c4ad
Marcel Hecko <maco@blava.net>
2012-08-30 11:19:22 +0300

    Update sql/mysql.sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,7 +1,6 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE DATABASE `slsp` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `slsp`;
 
 CREATE TABLE IF NOT EXISTS `categorys` (
